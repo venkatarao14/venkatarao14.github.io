5e0e3bd1e2de53a30d70755cf73cefd1bef47e8b
oxygenaatma <122196738+oxygenaatma@users.noreply.github.com>
2025-01-18 12:40:13 -0600

    header script

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const headerHTML = `<div class="navbar">
         <ul>
         <a href="introduction.html"> Introduction</a>
         <a href="medical_tests.html">Medical tests</a>    
-        <a href="Happylivingtips.html">Happy and healthy living tips</a>   
-        <a href="Weather_and_seasonal_effects.html">Weather </a>
+        <a href="happy_livingtips.html">Happy and healthy living tips</a>   
+        <a href="weather_and_seasonal_effects.html">Weather </a>
         <a href="waterintake.html">Water Intake </a>
-        <a href="Emotional wellness.html">Emotional wellness </a>
+        <a href="emotional_wellness.html">Emotional wellness </a>
         <a href="spiritual_focus.html">Spiritual focus </a>
         <a href="travelcare.html">Travel care</a>
             <div class="dropdown">
@@ -14,9 +14,9 @@ const headerHTML = `<div class="navbar">
                 </button>
                 <a href="falls.html">Falls </a>
                 <div class="dropdown-content">
-                    <a href="falls causes.html">Causes </a>
-                    <a href="falls prevention.html">Preventive tips </a>
-                    <a href="falls if.html">Falls if </a>
+                    <a href="falls_causes.html">Causes </a>
+                    <a href="falls_prevention.html">Preventive tips </a>
+                    <a href="falls_if.html">Falls if </a>
                 </div>
             </div>
         </ul>
